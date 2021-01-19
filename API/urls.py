f22ac260ba9da97b17860b7f925ab97878bf7e1d
Amirabbas_k <amirabbaskianpour@gmail.com>
2021-01-19 18:20:58 +0330

    get a specific post with API completed

--- API/urls.py
+++ API/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import collect_post
+from .views import collect_post , post_finder
 urlpatterns = [
-        path('collect_post/' , collect_post , name = "get_post_api_url" )
+        path('collect_post/' , collect_post , name = "get_post_api_url" ),
+        path('get_post/<int:id>' , post_finder , name = "open_post_api_url" )
 ]
--- API/views.py
+++ API/views.py
@@ -2,11 +2,23 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from Blog.models import Posts
 from .serializer import post_serializer
+from django.http import HttpResponse
 from rest_framework.response import Response
 # Create your views here.
 @api_view(["GET" , "POST"])
-def collect_post (request): # collect and send posts 
+def collect_post (request): # collect and send posts # Get all posts
     posts =  Posts.objects.all()
     serializer =  post_serializer(posts , many = True)
     return Response(serializer.data)
+
+@api_view(["GET"])
+def post_finder (request , id ): # find a special post
+    try:
+        post = Posts.objects.get(id =id)
+    except Posts.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = post_serializer(post)
+        return Response(serializer.data)
 
